4d3c87a55c5e291a4e9fab36d764793d73663f28
dnqv587 <1336047549@qq.com>
2024-11-30 18:17:38 +0800

    jz10

---
+++ 剑指offer/JZ10.斐波那契数列.cpp
@@ -0,0 +1,25 @@
+/**
+ * @nc app=nowcoder id=c6c7742f5ba7442aada113136ddea0c3 topic=13 question=11160 lang=C++
+ * 2024-11-17 22:26:54
+ * https://www.nowcoder.com/practice/c6c7742f5ba7442aada113136ddea0c3?tpId=13&tqId=11160
+ * [JZ10] 斐波那契数列
+ */
+
+/** @nc code=start */
+
+class Solution {
+public:
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 
+     * @param n int整型 
+     * @return int整型
+     */
+    int Fibonacci(int n) {
+        // write code here
+
+    }
+};
+
+/** @nc code=end */
